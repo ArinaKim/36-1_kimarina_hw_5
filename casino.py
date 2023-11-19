cf9e9b62bd242f214fe2f41e8e8df91b5e869212
ArinaKim <151456991+ArinaKim@users.noreply.github.com>
2023-11-20 00:24:24 +0600

    homework5

--- casino.py
+++ casino.py
@@ -1,21 +1,17 @@
 import configparser
 from game_logic import play_game, check_balance
 
-# Чтение начального капитала из файла настроек
 config = configparser.ConfigParser()
 config.read('settings.ini')
 initial_balance = int(config['Settings']['MY_MONEY'])
 
-# Инициализация переменных
 balance = initial_balance
 play_again = True
 
 while play_again:
-    # Получение ставки и выбранного слота от пользователя
     bet = int(input(f"Your balance: ${balance}. Enter your bet: "))
     chosen_slot = int(input("Choose a slot (1-30): "))
 
-    # Игра и проверка результата
     win, result, winning_slot = play_game(bet, chosen_slot)
 
     print(f"Winning slot: {winning_slot}")
@@ -25,7 +21,6 @@ while play_again:
         print(f"Sorry, you lost ${abs(result)}.")
         balance = check_balance(balance, result)
 
-    # Проверка баланса и предложение сыграть еще
     if balance <= 0:
         print("Game over. You're out of money.")
         play_again = False
@@ -37,5 +32,4 @@ while play_again:
         else:
             play_again = False
 
-# Вывод итогового баланса
 print(f"Your final balance: ${balance}")
